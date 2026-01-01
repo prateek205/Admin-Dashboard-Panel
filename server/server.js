642e1423fd5a9791ff1ca8ef7ab740b946fd04de
Prateek_Bahad <150145430+prateek205@users.noreply.github.com>
2026-01-01 22:23:14 +0530

    update changes: server/server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
 const connectDB = require('./config/db');
+import fs from "fs";
 
 // Load environment variables
 dotenv.config();
@@ -20,6 +21,10 @@ app.use(express.urlencoded({ extended: true }));
 // Serve static files from uploads directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+if (!fs.existsSync("uploads")) {
+  fs.mkdirSync("uploads");
+}
+
 // Routes
 app.use('/api/auth', require('./routes/authRoute'));
 app.use('/api/products', require('./routes/productRoute'));
@@ -29,6 +34,8 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Something went wrong!' });
 });
+
+
 
 const PORT = process.env.PORT || 5000;
 
