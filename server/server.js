c3468fd663fac12ce4344a37891d5a88e89b4a92
Prateek_Bahad <150145430+prateek205@users.noreply.github.com>
2026-01-02 17:04:37 +0530

    update changes: server/server.js

--- server/server.js
+++ server/server.js
@@ -14,23 +14,13 @@ connectDB();
 const app = express();
 
 // Middleware
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://admin-dashboard-panel-iota.vercel.app",
-];
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (Postman, curl)
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: [
+      "http://localhost:5173",
+      "https://admin-dashboard-panel-two.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
@@ -41,16 +31,16 @@ app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
-app.use("/api/auth", require("./routes/authRoute"));
-app.use("/api/products", require("./routes/productRoute"));
-
 // Serve static files from uploads directory
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 if (!fs.existsSync("uploads")) {
   fs.mkdirSync("uploads");
 }
+
+// Routes
+app.use("/api/auth", require("./routes/authRoute"));
+app.use("/api/products", require("./routes/productRoute"));
 
 // Error handling middleware
 app.use((err, req, res, next) => {
